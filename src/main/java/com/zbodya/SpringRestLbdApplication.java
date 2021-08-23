fd8071b62d1ba56e7015fd4bb362645758a0e7fa
Bohdan Zborovskyi <baga142536@gmail.com>
2021-08-23 10:55:56 +0200

    Start project

---
+++ src/main/java/com/zbodya/SpringRestLbdApplication.java
@@ -0,0 +1,13 @@
+package com.zbodya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestLbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestLbdApplication.class, args);
+	}
+
+}
